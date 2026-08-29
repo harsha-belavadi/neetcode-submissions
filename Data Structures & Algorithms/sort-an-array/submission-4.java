98aee38022952dc6c6050d7835dc44484a732442
harsha-belavadi <harshabelavadi@gmail.com>
2026-08-29 15:22:42 +0530

    Add: sort-an-array - submission-4

---
+++ Data Structures & Algorithms/sort-an-array/submission-4.java
@@ -0,0 +1,52 @@
+class Solution {
+    private int[] selectionsort(int[] nums) {
+        int n = nums.length;
+        for (int i=0; i<n; i++) {
+            int mini = i;
+            for (int j=i+1; j<n; j++) {
+                if (nums[j] < nums[mini]) {
+                    mini = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[mini];
+            nums[mini] = temp;
+        }
+        return nums;
+    }
+
+    private int[] bubblesort(int[] nums) {
+        int n = nums.length;
+        boolean didSwap = false;
+        for (int i=n-1; i>=0; i--) {
+            for (int j=0; j<i; j++) {
+                if (nums[j] > nums[j+1]) {
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                    didSwap = true;
+                }
+            }
+            if (!didSwap) { return nums; }
+        }
+        return nums;
+    }
+
+    private int[] insertionsort(int[] nums) {
+        int n = nums.length;
+        for (int i=0; i<n; i++) {
+            int j = i;
+            while (j > 0 && nums[j-1] > nums[j]) {
+                int temp = nums[j-1];
+                nums[j-1] = nums[j];
+                nums[j] = temp;
+                j--;
+            }
+        }
+        return nums;
+    }
+
+    public int[] sortArray(int[] nums) {
+        return insertionsort(nums);
+    }
+}
