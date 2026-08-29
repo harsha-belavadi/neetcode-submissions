3c533bae8c6a0c153ed9930a2302c89f21001c9a
harsha-belavadi <harshabelavadi@gmail.com>
2026-08-30 01:21:29 +0530

    Add: sort-an-array - submission-6

---
+++ Data Structures & Algorithms/sort-an-array/submission-6.java
@@ -0,0 +1,90 @@
+class Solution {
+    private int[] selectionsort(int[] nums) {
+        int n = nums.length;
+        for (int i=0; i<n; i++) {
+            int mini = i;
+            for (int j=i+1; j<n; j++) {
+                if (nums[j] < nums[mini]) {
+                    mini = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[mini];
+            nums[mini] = temp;
+        }
+        return nums;
+    }
+
+    private int[] bubblesort(int[] nums) {
+        int n = nums.length;
+        boolean didSwap = false;
+        for (int i=n-1; i>=0; i--) {
+            for (int j=0; j<i; j++) {
+                if (nums[j] > nums[j+1]) {
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                    didSwap = true;
+                }
+            }
+            if (!didSwap) { return nums; }
+        }
+        return nums;
+    }
+
+    private int[] insertionsort(int[] nums) {
+        int n = nums.length;
+        for (int i=0; i<n; i++) {
+            int j = i;
+            while (j > 0 && nums[j-1] > nums[j]) {
+                int temp = nums[j-1];
+                nums[j-1] = nums[j];
+                nums[j] = temp;
+                j--;
+            }
+        }
+        return nums;
+    }
+
+    private int[] quicksort(int[] nums) {
+        return quicksort(nums, 0, nums.length - 1);
+    }
+
+    private int[] quicksort(int[] nums, int lowIndex, int highIndex) {
+        if (lowIndex >= highIndex) {
+            return nums;
+        }
+
+        int pivot = nums[highIndex];
+
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex;
+
+        while (leftPointer < rightPointer) {
+            while (leftPointer < rightPointer && nums[leftPointer] <= pivot) {
+                leftPointer++;
+            }
+
+            while (leftPointer < rightPointer && nums[rightPointer] >= pivot) {
+                rightPointer--;
+            }
+
+            int temp = nums[leftPointer];
+            nums[leftPointer] = nums[rightPointer];
+            nums[rightPointer] = temp;
+        }
+        
+        int temp = nums[highIndex]; // Pivot
+        nums[highIndex] = nums[leftPointer];
+        nums[leftPointer] = temp;
+
+        quicksort(nums, lowIndex, leftPointer - 1);
+        quicksort(nums, leftPointer + 1, highIndex);
+
+        return nums;
+    }
+
+    public int[] sortArray(int[] nums) {
+        return quicksort(nums);
+    }
+}
