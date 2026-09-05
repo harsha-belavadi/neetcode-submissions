b1a336b960e99329b84e30da3daa19e753335866
harsha-belavadi <harshabelavadi@gmail.com>
2026-09-06 00:35:54 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minBuy = prices[0];
+        int maxProfit = 0;
+        for (int i=1; i<prices.length; i++) {
+            minBuy = Math.min(minBuy, prices[i-1]);
+            maxProfit = Math.max(maxProfit, prices[i] - minBuy);
+        }
+        return maxProfit;
+    }
+}
