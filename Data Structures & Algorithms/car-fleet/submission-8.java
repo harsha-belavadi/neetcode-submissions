f1499ef77c5a466d4a4c60eb23880ac7b63a3ca4
harsha-belavadi <harshabelavadi@gmail.com>
2026-09-11 23:58:58 +0530

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] cars = new int[n][2];
+        for (int i=0; i<n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+        Arrays.sort(cars, (a, b) -> Integer.compare(b[0], a[0]));
+
+        double[] timeToReachDest = new double[n];
+        for (int i=0; i<n; i++) {
+            timeToReachDest[i] = (double) (target - cars[i][0]) / cars[i][1];
+        }
+
+        Stack<Double> stack = new Stack<>();
+        for (double time : timeToReachDest) {
+            if (stack.isEmpty() || time > stack.peek()) {
+                stack.push(time);
+            }            
+        }
+
+        return stack.size();
+    }
+}
