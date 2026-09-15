f38bb0aa99b52ee305ca9e4e85f959842c3270ee
harsha-belavadi <harshabelavadi@gmail.com>
2026-09-15 20:48:51 +0530

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    private int lowerbound(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int lb = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= target) {
+                lb = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return lb;
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int lb = lowerbound(nums, target);
+        return lb == -1 ? nums.length : lb; 
+    }
+}
